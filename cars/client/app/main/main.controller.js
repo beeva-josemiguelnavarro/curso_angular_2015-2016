577662b5c2c03d939ef21ef6a15715e2b4d7e396
Jose Miguel <jmn8718@gmail.com>
2015-12-10 00:57:04 +0100

    added option to match requirements

--- cars/client/app/main/main.controller.js
+++ cars/client/app/main/main.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('carsApp')
-  .controller('MainCtrl',['$scope','$http',function($scope,$http){
+  .controller('MainCtrl',['$scope','$http','Modal',function($scope,$http,Modal){
     $scope.options = {};
     $scope.error = false;
-    $scope.redy = false;
+    $scope.ready = false;
     $scope.myData = {
       type:'',
       bodyEngine: '',
@@ -16,13 +16,14 @@ angular.module('carsApp')
       colorSport: '',
       extras: [],
       cp: '',
-      email: ''
+      email: '',
+      comments: ''
     }
 
     $http.get('/api/options').then(function(response){
       //console.log(response)
       $scope.options = response.data;
-      $scope.ready = true;
+      $scope.initialize()
     },function(error){
       //console.log(error);
       $scope.error = true;
@@ -30,45 +31,48 @@ angular.module('carsApp')
     });
 
     $scope.toggleExtra = function(extra){
-      //console.log(extra)
-      //console.log($scope.myData.extras.indexOf(extra))
       if($scope.myData.extras.indexOf(extra)>-1)
         $scope.myData.extras.splice($scope.myData.extras.indexOf(extra),1)
       else
         $scope.myData.extras.push(extra)
     }
 
+    $scope.initialize = function(){
+      $scope.ready = false
+
+      $scope.myData.type = 'normal'
+      $scope.myData.bodyEngine = $scope.options.bodyEngines[0].value
+      $scope.myData.engine = $scope.options.engines[0].value
+      $scope.myData.transmission = $scope.options.transmissions[0].value
+      $scope.myData.finish = $scope.options.finishes[0].value
+      $scope.myData.tires = $scope.options.tires[0].value
+      $scope.myData.color = $scope.options.colors[0].value
+      $scope.myData.colorSport = $scope.options.colorsSport[0].value
+      $scope.myData.cp= '',
+      $scope.myData.email= '',
+      $scope.myData.comments= ''
+
+      $scope.ready = true
+    }
+
     $scope.sendForm = function(){
       $http.post('/api/customers',$scope.myData).then(function(data){
         console.log(data)
+        $scope.initialize()
       },function(error){
         console.log(error)
       })
     }
 
     $scope.$watch('myData.type',function(newValue){
-      //$scope.myData = {
-      //  type:newValue,
-      //  bodyEngine: '',
-      //  engine: '',
-      //  transmission: '',
-      //  finish: '',
-      //  tires: '',
-      //  color: '',
-      //  colorSport: '',
-      //  extras: [],
-      //  cp: '',
-      //  email: ''
-      //}
-
-      $scope.myData.bodyEngine = '';
-      $scope.myData.engine = '';
-      $scope.myData.transmission = '';
-      $scope.myData.finish = '';
-      $scope.myData.tires = '';
-      $scope.myData.color = '';
-      $scope.myData.colorSport = '';
-      $scope.myData.extras = [];
-
+      if(newValue=='sport'){
+        $scope.myData.bodyEngine = $scope.options.bodyEngines[2].value
+        $scope.myData.engine = $scope.options.engines[3].value
+        $scope.myData.transmission = $scope.options.transmissions[0].value
+        $scope.myData.finish = $scope.options.finishes[0].value
+        $scope.myData.tires = $scope.options.tires[3].value
+        $scope.myData.colorSport = $scope.options.colorsSport[0].value
+      }
     });
+
   }]);
