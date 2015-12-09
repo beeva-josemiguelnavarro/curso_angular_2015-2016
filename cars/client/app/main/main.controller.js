8cd06cd21fc819b5637d7266823ace58dde6c26d
Jose Miguel <josemiguel.navarro@bbva.com>
2015-12-09 09:03:46 +0100

    added validations and extras and send form

--- cars/client/app/main/main.controller.js
+++ cars/client/app/main/main.controller.js
@@ -20,14 +20,31 @@ angular.module('carsApp')
     }
 
     $http.get('/api/options').then(function(response){
-      console.log(response)
+      //console.log(response)
       $scope.options = response.data;
       $scope.ready = true;
     },function(error){
-      console.log(error);
+      //console.log(error);
       $scope.error = true;
       $scope.ready = true;
     });
+
+    $scope.toggleExtra = function(extra){
+      //console.log(extra)
+      //console.log($scope.myData.extras.indexOf(extra))
+      if($scope.myData.extras.indexOf(extra)>-1)
+        $scope.myData.extras.splice($scope.myData.extras.indexOf(extra),1)
+      else
+        $scope.myData.extras.push(extra)
+    }
+
+    $scope.sendForm = function(){
+      $http.post('/api/customers',$scope.myData).then(function(data){
+        console.log(data)
+      },function(error){
+        console.log(error)
+      })
+    }
 
     $scope.$watch('myData.type',function(newValue){
       //$scope.myData = {
@@ -51,7 +68,7 @@ angular.module('carsApp')
       $scope.myData.tires = '';
       $scope.myData.color = '';
       $scope.myData.colorSport = '';
-      $scope.myData.extras = '';
+      $scope.myData.extras = [];
 
     });
   }]);
