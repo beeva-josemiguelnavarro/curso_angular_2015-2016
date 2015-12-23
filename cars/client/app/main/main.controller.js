4996db791daf04141499e08be5ec17a68e287c9d
Jose Miguel <josemiguel.navarro@bbva.com>
2015-12-23 12:37:14 +0100

    added modal and validation messages

--- cars/client/app/main/main.controller.js
+++ cars/client/app/main/main.controller.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('carsApp')
-  .controller('MainCtrl',['$scope','$http','Modal',function($scope,$http,Modal){
+  .controller('MainCtrl',['$scope','$http',function($scope,$http){
     $scope.options = {};
+
     $scope.error = false;
     $scope.ready = false;
+
+    $scope.sendingForm = false;
+    $scope.successForm = false;
+    $scope.errorForm = false;
+
+    $scope.showModal = false;
+
+    $scope.closeModal = function(){
+      $scope.showModal = false;
+      $scope.sendingForm = false;
+      $scope.successForm = false;
+      $scope.errorForm = false;
+      $scope.initialize()
+    }
+
     $scope.myData = {
       type:'',
       bodyEngine: '',
@@ -27,7 +43,6 @@ angular.module('carsApp')
     },function(error){
       //console.log(error);
       $scope.error = true;
-      $scope.ready = true;
     });
 
     $scope.toggleExtra = function(extra){
@@ -36,6 +51,7 @@ angular.module('carsApp')
       else
         $scope.myData.extras.push(extra)
     }
+
 
     $scope.initialize = function(){
       $scope.ready = false
@@ -48,30 +64,49 @@ angular.module('carsApp')
       $scope.myData.tires = $scope.options.tires[0].value
       $scope.myData.color = $scope.options.colors[0].value
       $scope.myData.colorSport = $scope.options.colorsSport[0].value
+      $scope.myData.extras = []
       $scope.myData.cp= '',
       $scope.myData.email= '',
       $scope.myData.comments= ''
 
-      $scope.ready = true
+      setTimeout(function(){
+        $scope.ready = true;
+        $scope.$apply();
+      },0);
     }
 
     $scope.sendForm = function(){
+      $scope.showModal = true;
+      $scope.sendingForm = true;
+      $scope.errorForm = false;
+      $scope.successForm = false;
+
       $http.post('/api/customers',$scope.myData).then(function(data){
         console.log(data)
+        $scope.sendingForm = false;
+        $scope.successForm = true;
         $scope.initialize()
       },function(error){
+        $scope.sendingForm = false;
+        $scope.errorForm = true;
         console.log(error)
       })
     }
 
     $scope.$watch('myData.type',function(newValue){
       if(newValue=='sport'){
-        $scope.myData.bodyEngine = $scope.options.bodyEngines[2].value
-        $scope.myData.engine = $scope.options.engines[3].value
-        $scope.myData.transmission = $scope.options.transmissions[0].value
-        $scope.myData.finish = $scope.options.finishes[0].value
-        $scope.myData.tires = $scope.options.tires[3].value
+        $scope.myData.bodyEngine = _.find($scope.options.bodyEngines, {type: newValue}).value;
+        $scope.myData.engine = _.find($scope.options.engines, {type: newValue}).value;
+        $scope.myData.transmission = _.find($scope.options.transmissions, {type: newValue}).value;
+        $scope.myData.finish = _.find($scope.options.finishes, {type: newValue}).value;
+        $scope.myData.tires = _.find($scope.options.tires, {type: newValue}).value;
         $scope.myData.colorSport = $scope.options.colorsSport[0].value
+        for (var extra in $scope.options.extras) {
+          if($scope.myData.extras.indexOf($scope.options.extras[extra].value)<0)
+            $scope.myData.extras.push($scope.options.extras[extra].value)
+        }
+      } else {
+        $scope.myData.colorSport = '';
       }
     });
 
